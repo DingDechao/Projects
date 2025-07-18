cf0a0b1f266047c92feae1e5812df15335c6a687
DING DECHAO-MAC15 <dingdechao_0486@163.com>
2025-07-18 14:08:39 +0800

    Complete one of todo list

--- algorithm/src/main/java/com/ddc/algorithm/mashibin/BitOperationTest0.java
+++ algorithm/src/main/java/com/ddc/algorithm/mashibin/BitOperationTest0.java
@@ -1,5 +1,8 @@
 package com.ddc.algorithm.mashibin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BitOperationTest0 {
     public static void main(String[] args) {
 
@@ -9,14 +12,29 @@ public class BitOperationTest0 {
         swapNumberWithoutVar(3, 12);
 
         printOddNumberWithinArray(new int[]{1, 1, 3});
+
+
+        int[] array = new int[]{4, 3, 4, 2, 2, 2, 4, 1, 1, 1, 3, 3, 1, 1, 1, 4, 2, 2};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a : array) {
+            if (map.containsKey(a)) {
+                int times = map.get(a);
+                map.put(a, ++(times));
+            } else {
+                map.put(a, 1);
+            }
+        }
+        System.out.println(map);
+
+        print2OddNumberWithinArray(array);
     }
 
-    public static void swapNumberWithoutVar(int a , int b) {
-        a = a ^b;
+    public static void swapNumberWithoutVar(int a, int b) {
+        a = a ^ b;
         b = a ^ b;
-        a = a ^b;
+        a = a ^ b;
 
-        System.out.println("a=" + a + ", b = "+b);
+        System.out.println("a=" + a + ", b = " + b);
     }
 
     //一个数组中只有一个数字出现奇数次数
@@ -30,9 +48,25 @@ public class BitOperationTest0 {
 
     //一个数组中只有两个数字出现奇数次数
     public static void print2OddNumberWithinArray(int[] array) {
-        
+        int number = 0;
+        for (var i = 0; i < array.length; i++) {
+            number ^= array[i];
+        }
+        // 此时,number等于这两个奇数异或的结果
+        //接下来, 找出 number 32位中最后一个1的位置
+        // number  00110010110111000
+        // ~number 01001101001000111 + 1 =
+        // +1      01001101001001000
+        // lastOne=00000000000001000
+        int lastOne = number & (-number);
+        int oneOfOdd = 0;
+        for (var i = 0; i < array.length; i++) {
+            if ((array[i] & lastOne) == 1) {
+                oneOfOdd ^= array[i];
+            }
+        }
+        System.out.println("One is " + oneOfOdd + ", another is " + (number ^ oneOfOdd));
     }
-    // 找出自己二进制的第一位1
 
 
     public static void negationOperation(int number) {
