9ddaa38dbb642166897ca2c26eaa19987f1ecb63
DING DECHAO-LEGION-DESKTOP <dingdechao_0486@163.com>
2025-09-12 05:13:31 +0800

    Enhance bubble sort

--- algorithm/src/main/java/com/ddc/algorithm/sort/BubbleSort.java
+++ algorithm/src/main/java/com/ddc/algorithm/sort/BubbleSort.java
@@ -6,15 +6,27 @@ public class BubbleSort {
     //冒泡排序
     //两个两个比较，把大的往右边移动
     //第一轮结束 最大的肯定在最右边
+    // 一共需要遍历元素数量减一轮，遍历一次确定一个，遍历N-1轮,那么N-1个元素就有序了，最后一个元素不用遍历了就在正确的位置上了
     //。。。
 
 
     public void bubbleSort(int[] arr) {
-        for (var end = arr.length - 1 ; end > 0; end--) {
-            for (var j = 1; j <= end; j++) {
-                if(arr[j] < arr[j-1]) {
-                    swap(arr, j, j-1);
+        // 优化思路2：每一轮比较时，记录最后一次交换的位置，作为下一轮比较的结束位置
+        int lastExchangeIndex = 0;
+        int sortBorder = arr.length - 1;
+        for (var end = arr.length - 1; end > 0; end--) {
+            // 优化思路1：如果某一轮没有进行交换，说明数组已经有序，可以提前结束排序
+            boolean swapped = false;
+            for (var j = 1; j <= sortBorder; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                    swapped = true;
+                    lastExchangeIndex = j - 1;
                 }
+            }
+            sortBorder = lastExchangeIndex;
+            if (!swapped) {
+                break;
             }
         }
     }
@@ -30,10 +42,20 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {8,7,5,3,1,9};
         BubbleSort bubbleSort = new BubbleSort();
+        int[] arr = {8, 7, 5, 3, 1, 9};
         bubbleSort.printIntArray(arr);
         bubbleSort.bubbleSort(arr);
         bubbleSort.printIntArray(arr);
+
+        int[] arr2 = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
+        bubbleSort.printIntArray(arr2);
+        bubbleSort.bubbleSort(arr2);
+        bubbleSort.printIntArray(arr2);
+
+        int[] arr3 = new int[]{3, 4, 2, 1, 5 ,6, 7, 8   };
+        bubbleSort.printIntArray(arr3);
+        bubbleSort.bubbleSort(arr3);
+        bubbleSort.printIntArray(arr3);
     }
 }
