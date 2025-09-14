7aa102e46ab8af9e4caa19e52620c1ce93ea50ce
DING DECHAO-MAC15 <dingdechao_0486@163.com>
2025-09-15 02:06:43 +0800

    return cycle length for linkedlist

--- algorithm/src/main/java/com/ddc/algorithm/linkedlist/CycleEntryPoint.java
+++ algorithm/src/main/java/com/ddc/algorithm/linkedlist/CycleEntryPoint.java
@@ -1,11 +1,8 @@
 package com.ddc.algorithm.linkedlist;
 
-import java.util.HashSet;
-import java.util.Set;
+public class CycleEntryPoint {
 
-public class IsThereACycle {
-    // 判断链表是否有环
-
+    // 链表是不是有环, 如果有环, 求出环的入口节点
     private static class ListNode {
         int value;
         ListNode next;
@@ -15,33 +12,13 @@ public class IsThereACycle {
         }
     }
 
-    // 可以用hash表记录每次便利的节点,如果遍历的节点已经存在于hash表中,则说明有环
-    // 不是最佳方法
-    // 时间复杂度O(N),空间复杂度O(N)
-     public boolean hasCycle1(ListNode head) {
-         if (head == null || head.next == null) {
-             return false;
-         }
-         Set<ListNode> set = new HashSet<>();
-         ListNode current = head;
-         while (current != null) {
-             if (set.contains(current)) {
-                 return true;
-             }
-             set.add(current);
-             current = current.next;
-         }
-         return false;
-     }
-
-
     // 快慢指针, 实际上是追及问题
     // 慢指针每次走一步,快指针每次走两步,如果有环,快指针一定会追上慢指针
     // 这个是最佳方法
     // 时间复杂度O(N),空间复杂度O(1)
-    public boolean hasCycle(ListNode head) {
+    public int cycleLength(ListNode head) {
         if (head == null || head.next == null) {
-            return false;
+            return 0;
         }
         ListNode slow = head;
         ListNode fast = head;
@@ -49,15 +26,21 @@ public class IsThereACycle {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
-                return true;
+                // 有环, 计算环的长度
+                int length = 1;
+                ListNode current = slow.next;
+                while (current != slow) {
+                    length++;
+                    current = current.next;
+                }
+                return length;
             }
         }
-        return false;
+        return 0;
     }
 
     public static void main(String[] args) {
-        IsThereACycle isThereACycle = new IsThereACycle();
-
+        CycleEntryPoint cycleEntryPoint = new CycleEntryPoint();
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
         ListNode node3 = new ListNode(3);
@@ -70,10 +53,11 @@ public class IsThereACycle {
         node4.next = node5;
         //node5.next = node3; // 形成环
 
-        System.out.println(isThereACycle.hasCycle(node1)); // false
+        System.out.println(cycleEntryPoint.cycleLength(node1)); // 0
 
         node5.next = node2; // 形成环
-        System.out.println(isThereACycle.hasCycle(node1)); // true
+        System.out.println(cycleEntryPoint.cycleLength(node1)); // 4
     }
+
 
 }
